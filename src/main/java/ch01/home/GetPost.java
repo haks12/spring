455f30653a06983a5680dad9e490bc4e54fd98d2
unknown <a@a.com>
2022-09-06 10:36:16 +0900

    '0906'

--- src/main/java/ch01/home/GetPost.java
+++ src/main/java/ch01/home/GetPost.java
@@ -2,8 +2,8 @@ package ch01.home;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.rmi.ServerException;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +13,13 @@ import jakarta.servlet.http.HttpServletResponse;
 public class GetPost extends HttpServlet {
 	@Override
 	public void service(HttpServletRequest req, HttpServletResponse res) 
-		throws ServletException, IOException {
+			throws ServerException, IOException {
 		int num1 = 1;
 		int num2 = 2;
 		
 		res.setContentType("text/html; charset=utf-8");
 		PrintWriter out = res.getWriter();
 		out.println("<h2>" + req.getMethod() + "</h2>");
-		out.printf("<p>%d + %d = %d</p>", num1, num2, num1 + num2);
+		out.printf("<p>%d + %d = %d</p>", num1, num2, num1 + num2);		
 	}
 }
